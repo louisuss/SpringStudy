2b60b2423a68eb956dd47e4e3e425184f3ac2f77
louis <dobi1115@gmail.com>
2020-09-27 23:23:24 +0900

    Spring in Action - Studied Chap2

---
+++ src/test/java/com/dobi/tacos/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dobi.tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
